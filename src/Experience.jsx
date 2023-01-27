02cf5895250c38101e6d0c09407961ea76349cad
James Hagood <cardfaux@tutanota.com>
2023-01-27 16:52:43 -0600

    added a collar

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,65 +1,68 @@
-import { shaderMaterial, Sparkles, Center, useTexture, useGLTF, OrbitControls } from '@react-three/drei'
-import * as THREE from 'three'
-import { useFrame, extend } from '@react-three/fiber'
-import { useRef } from 'react'
-import portalVertexShader from './shaders/portal/vertex.glsl'
-import portalFragmentShader from './shaders/portal/fragment.glsl'
+import { shaderMaterial, Sparkles, Center, useTexture, useGLTF, OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+import { useFrame, extend } from '@react-three/fiber';
+import { useRef } from 'react';
+import portalVertexShader from './shaders/portal/vertex.glsl';
+import portalFragmentShader from './shaders/portal/fragment.glsl';
 
 const PortalMaterial = shaderMaterial(
-    {
-        uTime: 0,
-        uColorStart: new THREE.Color('#ffffff'),
-        uColorEnd: new THREE.Color('#000000')
-    },
-    portalVertexShader,
-    portalFragmentShader
-)
+  {
+    uTime: 0,
+    uColorStart: new THREE.Color('#ffffff'),
+    uColorEnd: new THREE.Color('#000000'),
+  },
+  portalVertexShader,
+  portalFragmentShader
+);
 
-extend({ PortalMaterial })
+extend({ PortalMaterial });
 
-export default function Experience()
-{
-    const { nodes } = useGLTF('./model/portal.glb')
+export default function Experience() {
+  const { nodes } = useGLTF('./model/portal.glb');
 
-    const bakedTexture = useTexture('./model/baked.jpg')
-    bakedTexture.flipY = false
-    
-    const portalMaterial = useRef()
+  const bakedTexture = useTexture('./model/baked.jpg');
+  bakedTexture.flipY = false;
 
-    useFrame((state, delta) =>
-    {
-        portalMaterial.current.uTime += delta
-    })
+  const portalMaterial = useRef();
 
-    return <>
+  useFrame((state, delta) => {
+    portalMaterial.current.uTime += delta;
+  });
 
-        <OrbitControls makeDefault />
+  const model = useGLTF('./model/chocker.glb');
 
-        <Center>
-            <mesh geometry={ nodes.baked.geometry }>
-                <meshBasicMaterial map={ bakedTexture } />
-            </mesh>
+  console.log(model);
 
-            <mesh geometry={ nodes.poleLightA.geometry } position={ nodes.poleLightA.position }>
-                <meshBasicMaterial color="#ffffe5" />
-            </mesh>
+  return (
+    <>
+      <OrbitControls makeDefault />
 
-            <mesh geometry={ nodes.poleLightB.geometry } position={ nodes.poleLightB.position }>
-                <meshBasicMaterial color="#ffffe5" />
-            </mesh>
+      <Center>
+        <mesh geometry={nodes.baked.geometry}>
+          <meshBasicMaterial map={bakedTexture} />
+        </mesh>
 
-            <mesh geometry={ nodes.portalLight.geometry } position={ nodes.portalLight.position } rotation={ nodes.portalLight.rotation }>
-                <portalMaterial ref={ portalMaterial } />
-            </mesh>
+        <mesh geometry={nodes.poleLightA.geometry} position={nodes.poleLightA.position}>
+          <meshBasicMaterial color='#ffffe5' />
+        </mesh>
 
-            <Sparkles
-		        size={ 6 }
-                scale={ [ 4, 2, 4 ] }
-                position-y={ 1 }
-                speed={ 0.2 }
-                count={ 40 }
-            />
-        </Center>
+        <mesh geometry={nodes.poleLightB.geometry} position={nodes.poleLightB.position}>
+          <meshBasicMaterial color='#ffffe5' />
+        </mesh>
 
+        <mesh
+          geometry={nodes.portalLight.geometry}
+          position={nodes.portalLight.position}
+          rotation={nodes.portalLight.rotation}
+        >
+          <portalMaterial ref={portalMaterial} />
+        </mesh>
+
+        <Sparkles size={6} scale={[4, 2, 4]} position-y={1} speed={0.2} count={40} />
+        <primitive object={model.scene} scale={0.2} position={[0, 0.1, -0.5]} rotation={[0, 0, 1]} />
+      </Center>
+
+      {/* <primitive object={model.scene} scale={0.2} /> */}
     </>
+  );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,22 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Experience from './Experience.jsx'
+import './style.css';
+import ReactDOM from 'react-dom/client';
+import { Canvas } from '@react-three/fiber';
+import Experience from './Experience.jsx';
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
-    <Canvas
-        flat
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 50,
-            position: [ 1, 2, 6 ]
-        } }
-    >
-        <color args={ [ '#030202' ] } attach="background" />
-        <Experience />
-    </Canvas>
-)
+  <Canvas
+    flat
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 50,
+      position: [1, 2, 6],
+    }}
+  >
+    <color args={['#030202']} attach='background' />
+    {/* <color args={['#FFFFFF']} attach='background' /> */}
+    <Experience />
+  </Canvas>
+);
